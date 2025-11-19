fbf291db46dcfb67b5f14ec7f32aa01223c93760
amirrezajm <amirreza.jolani@yahoo.com>
2025-11-19 22:12:15 +0100

    fix all the error while building @portfolio1.0.1

--- src/components/LogoLoop.tsx
+++ src/components/LogoLoop.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-
+import Image from 'next/image';
 export type LogoItem =
   | {
       node: React.ReactNode;
@@ -338,7 +338,7 @@ export const LogoLoop = React.memo<LogoLoopProps>(
             {(item as any).node}
           </span>
         ) : (
-          <img
+          <Image
             className={cx(
               'h-[var(--logoloop-logoHeight)] w-auto block object-contain',
               '[-webkit-user-drag:none] pointer-events-none',
@@ -348,7 +348,6 @@ export const LogoLoop = React.memo<LogoLoopProps>(
                 'transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] group-hover/item:scale-120'
             )}
             src={(item as any).src}
-            srcSet={(item as any).srcSet}
             sizes={(item as any).sizes}
             width={(item as any).width}
             height={(item as any).height}
--- src/types/portfolio.types.ts
+++ src/types/portfolio.types.ts
@@ -2,14 +2,6 @@ export interface CardHeaderProps {
     title: string;
 }
 
-export interface FeatureCardProps {
-  title: string;
-  description: string;
-  icon?: React.ReactNode;
-  delay?: number;
-  hoverBorderColor?: string;
-}
-
 export  interface MusicCardProps {
   category: string;
   title: string;
